36883cf933bc5d4e7bd1b80641b56c47864d16ff
Ali Afkhami <ali.afkhamiii@gmail.com>
2025-06-04 21:57:04 +0330

    feat: add transformer encoder

---
+++ transformer/encoder.py
@@ -0,0 +1,35 @@
+from torch import nn
+import torch
+
+from positional_encoding import SinusoidalPE
+from encoder_block import EncoderBlock
+from embedding import Embedding
+
+
+class TransformerEncoder(nn.Module):
+
+    def __init__(self, vocab_size, embed_dim, model_dim, num_hiddens, ff_dim, num_heads, pe_max_len=1000, dropout=0.1,
+                 bias=False, num_layers=8):
+        super().__init__()
+
+        self.num_layers = num_layers
+
+        self.embedding = Embedding(vocab_size, embed_dim, model_dim)
+        self.PE = SinusoidalPE(num_hiddens, dropout=dropout, max_len=pe_max_len)
+
+        # construct num_layer encoder layers
+        self.layers = nn.Sequential()
+        for i in range(num_layers):
+            self.layers.add_module(f"block{i + 1}",
+                                   EncoderBlock(num_hiddens, ff_dim, num_heads, model_dim, dropout, bias)
+                                   )
+
+    def forward(self, x, valid_lens):
+        x = self.PE(self.embedding(x))
+
+        self.attention_weights = []
+        for i, layer in enumerate(self.layers):
+            x = layer(x, valid_lens)
+            self.attention_weights.append(layer.attention.attention_weights)
+
+        return x
