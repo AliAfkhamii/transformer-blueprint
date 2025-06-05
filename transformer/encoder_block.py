423cc03b378fa6505ef06071f60728c0d8024d6a
Ali Afkhami <ali.afkhamiii@gmail.com>
2025-06-05 18:55:34 +0330

    fix: add newly instantiated LayerNorm with AddNorm for each sublayer

--- transformer/encoder_block.py
+++ transformer/encoder_block.py
@@ -7,16 +7,17 @@ from addnorm import AddNorm
 
 class EncoderBlock(nn.Module):
 
-    def __init__(self, num_hiddens, ff_dim, num_heads, d_model, dropout=0.1, bias=False):
+    def __init__(self, num_hiddens, ff_dim, num_heads, model_dim, dropout=0.1, bias=False):
         super().__init__()
 
-        self.attention = MultiHeadAttention(num_hiddens, num_heads, d_model, dropout, bias)
-        self.ffn = FeedForwardNetwork(d_model, ff_dim)
-        self.add_norm = AddNorm(d_model, dropout)
+        self.attention = MultiHeadAttention(num_hiddens, num_heads, model_dim, dropout, bias)
+        self.ffn = FeedForwardNetwork(model_dim, ff_dim)
+        self.add_norm1 = AddNorm(model_dim, dropout)
+        self.add_norm2 = AddNorm(model_dim, dropout)
 
-    def forward(self, x):
+    def forward(self, x, valid_len):
         # attention sublayer followed by residual connection and Layer normalization
-        y = self.add_norm(x, self.attention(x))
+        y = self.add_norm1(x, self.attention(queries=x, keys=x, values=x, valid_len=valid_len))
 
         # feed-forward sublayer followed by residual connection and Layer normalization
-        return self.add_norm(y + self.ffn(y))
+        return self.add_norm2(y + self.ffn(y))
